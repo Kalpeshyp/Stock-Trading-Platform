c77831c17e34b84a5a78e1d133a6654de328ec99
Kalpeshyp <kalpeshyp491@gmail.com>
2026-03-06 13:38:19 +0530

    create landing page of webapp

--- frontend/src/landing_page/products/ProductPage.jsx
+++ frontend/src/landing_page/products/ProductPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Hero from './Hero';
-import LeftSection from './LeftSection'
-import RightSection from './RightSection'
-import Universe from './Universe'
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
 
 function ProductPage() {
   return (
@@ -17,7 +17,12 @@ function ProductPage() {
         googlePlay="media\images\googlePlayBadge.svg"
         appStore="media\images\appstoreBadge.svg"
       />
-      <RightSection />
+      <RightSection
+        imageUrl="media\images\console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
       <LeftSection
         imageUrl="media\images\coin.png"
         productName="Coin"
@@ -28,7 +33,27 @@ function ProductPage() {
         appStore="media\images\appstoreBadge.svg"
       />
 
-      <RightSection />
+      <RightSection
+        imageUrl="media\images\kite.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+      />
+      <LeftSection
+        imageUrl="media/images\varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay="media\images\googlePlayBadge.svg"
+        appStore="media\images\appstoreBadge.svg"
+      />
+      <p className="fs-5 text-center p-5">
+        Want to know more about our technology stack? Check out the{" "}
+        <a href=""> Zerodha.tech</a>
+        blog.
+      </p>
+
       <Universe />
     </>
   );
--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 
 function CreateTicket() {
-  return (<div>
-    <h1></h1>
-  </div> );
+  return (
+    <div></div>
+  );
 }
 
 export default CreateTicket;
